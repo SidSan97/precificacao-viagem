fe13d7ce225ac503bd4fe85db97c781b093c6568
Sidnei Santiago <sidnei1.8santiago@hotmail.com>
2026-06-05 21:40:24 -0300

    feat: add rota para cotação

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuoteController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/test', function () {
-    return response()->json([
-        'message' => 'Hello World',
-    ]);
-});
+Route::post('/quotes', [QuoteController::class, 'calculate']);
