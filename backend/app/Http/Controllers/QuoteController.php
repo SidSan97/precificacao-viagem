a013ca20070c57d16334086b15795c551d4c651b
Sidnei Santiago <sidnei1.8santiago@hotmail.com>
2026-06-05 21:03:36 -0300

    feat: criando injerção de dependencia do service de calculo

--- backend/app/Http/Controllers/QuoteController.php
+++ backend/app/Http/Controllers/QuoteController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\QuoteCalculatorService;
 
 class QuoteController extends Controller
 {
-    //
+    private QuoteCalculatorService $quoteCalculatorService;
+
+    public function __construct(QuoteCalculatorService $quoteCalculatorService)
+    {
+        $this->quoteCalculatorService = $quoteCalculatorService;
+    }
+
+    public function calculate(Request $request)
+    {
+       
+    }
 }
