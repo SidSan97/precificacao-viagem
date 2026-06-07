58b7585da16f24891af4b34334e715ba2274f527
Sidnei Santiago <sidnei1.8santiago@hotmail.com>
2026-06-06 22:56:58 -0300

    feat: criando rota para listar cotações

--- backend/app/Http/Controllers/QuoteController.php
+++ backend/app/Http/Controllers/QuoteController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateQuoteRequest;
+use App\Http\Resources\QuoteGroupResource;
 use App\Http\Resources\QuoteResultResource;
 use App\Services\QuoteCalculatorService;
+use App\Repositories\QuoteRepository;
 
 class QuoteController extends Controller
 {
-    private QuoteCalculatorService $quoteCalculatorService;
-
-    public function __construct(QuoteCalculatorService $quoteCalculatorService)
+    private $quoteCalculatorService;
+    private $quoteRepository;
+    public function __construct(QuoteCalculatorService $quoteCalculatorService, QuoteRepository $quoteRepository)
     {
         $this->quoteCalculatorService = $quoteCalculatorService;
+        $this->quoteRepository = $quoteRepository;
+    }
+
+    public function index()
+    {
+        $quotes = $this->quoteRepository->all();
+
+        return QuoteGroupResource::collection($quotes);
     }
 
     public function calculate(CreateQuoteRequest $request)
@@ -20,6 +30,8 @@ class QuoteController extends Controller
         $data = $request->validated();
 
         $quote = $this->quoteCalculatorService->calculate($data);
+
+        $this->quoteRepository->create($data, $quote);
 
         return new QuoteResultResource($quote);
     }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuoteController;
 
 Route::post('/quotes', [QuoteController::class, 'calculate']);
+Route::get('/quotes', [QuoteController::class, 'index']);
